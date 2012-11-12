b87637d7fc7bc1e82aa01e03b853462dac8d9884
Ulrich Schmidt <admin@deruli.de>
2012-11-12 19:47:25 +0100

    Default Ribbon-Tab korrigiert

--- src/uliedit_gui.py
+++ src/uliedit_gui.py
@@ -68,7 +68,7 @@ class MainFrame ( wx.Frame ):
 		self.m_panel1.SetSizer( bSizer2 )
 		self.m_panel1.Layout()
 		bSizer2.Fit( self.m_panel1 )
-		self.ribbons.AddPage( self.m_panel1, u"START", False )
+		self.ribbons.AddPage( self.m_panel1, u"START", True )
 		self.m_panel2 = wx.Panel( self.ribbons, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
 		bSizer3 = wx.BoxSizer( wx.HORIZONTAL )
 		
@@ -199,7 +199,7 @@ class MainFrame ( wx.Frame ):
 		self.m_panel7.SetSizer( bSizer20 )
 		self.m_panel7.Layout()
 		bSizer20.Fit( self.m_panel7 )
-		self.ribbons.AddPage( self.m_panel7, u"TOOLS", True )
+		self.ribbons.AddPage( self.m_panel7, u"TOOLS", False )
 		self.m_panel8 = wx.Panel( self.ribbons, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
 		bSizer19 = wx.BoxSizer( wx.HORIZONTAL )
 		
