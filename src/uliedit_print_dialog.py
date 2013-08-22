c9370498e3e2ffab0a3afe172c2e6ce16b23f16d
Ulrich Schmidt <admin@deruli.de>
2013-08-22 18:09:08 +0200

    PrFile32 entfernt

--- src/uliedit_print_dialog.py
+++ src/uliedit_print_dialog.py
@@ -22,7 +22,7 @@ class PrintDialog:
                 if enviroment_command:
                         self.form.txtCommand.SetValue(enviroment_command)
                 elif sys.platform == 'win32':
-                    self.form.txtCommand.SetValue("PrFile32.exe /q %1")
+                    self.form.txtCommand.SetValue("notepad /P %1")
                 else:
                     self.form.txtCommand.SetValue("lpr -o portrait -J \"$title\" %1")
                     
